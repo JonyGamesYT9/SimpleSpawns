aea0c0ed010a33f6811a573a58c269a70e324d3e
Jony <68771903+JonyGamesYT9@users.noreply.github.com>
2021-07-25 14:50:42 -0500

    update SimpleSpawns.php

--- src/JonyGamesYT9/SimpleSpawns/SimpleSpawns.php
+++ src/JonyGamesYT9/SimpleSpawns/SimpleSpawns.php
@@ -1,1 +1,66 @@
+<?php
 
+namespace JonyGamesYT9\SimpleSpawns;
+
+use JonyGamesYT9\SimpleSpawns\provider\YamlProvider;
+use JonyGamesYT9\SimpleSpawns\commands\LobbyCommand;
+use JonyGamesYT9\SimpleSpawns\commands\SetLobbyCommand;
+use pocketmine\plugin\PluginBase;
+
+/**
+* Class SimpleSpawns
+* @package JonyGamesYT9\SimpleSpawns
+*/
+class SimpleSpawns extends PluginBase
+{
+
+  /** @var SimpleSpawns $instance */
+  private static $instance;
+
+  /** @var YamlProvider $provider */
+  private $provider;
+
+  /**
+  * @return void
+  */
+  public function onLoad(): void
+  {
+    $this->saveResource("config.yml");
+    self::$instance = $this;
+    $this->provider = new YamlProvider($this);
+  }
+
+  /**
+  * @return void
+  */
+  public function onEnable(): void
+  {
+    $provider = $this->getYamlProvider();
+    if ($provider->getConfigVersion() === 1) {
+      $commands = [new LobbyCommand($this),
+        new SetLobbyCommand($this)];
+      foreach ($commands as $command) {
+        $this->getServer()->getCommandMap()->register("simplespawns", $command);
+      }
+    } else {
+      $this->getLogger()->error("SimpleSpawns: Error in config.yml please delete file and restart server!");
+      $this->getServer()->getPluginManager()->disablePlugin($this);
+    }
+  }
+
+  /**
+  * @return YamlProvider
+  */
+  public function getYamlProvider(): YamlProvider
+  {
+    return $this->provider;
+  }
+
+  /**
+  * @return SimpleSpawns
+  */
+  public static function getInstance(): SimpleSpawns
+  {
+    return self::$instance;
+  }
+}
