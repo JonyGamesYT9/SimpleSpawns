63f93eede63550f3a0486c5c243bc2636b68aeeb
Jony <68771903+JonyGamesYT9@users.noreply.github.com>
2021-07-25 14:23:32 -0500

    update SetLobbyCommand.php

--- src/JonyGamesYT9/SimpleSpawns/commands/SetLobbyCommand.php
+++ src/JonyGamesYT9/SimpleSpawns/commands/SetLobbyCommand.php
@@ -1,1 +1,63 @@
+<?php
 
+namespace JonyGamesYT9\SimpleSpawns\commands;
+
+use JonyGamesYT9\SimpleSpawns\SimpleSpawns;
+use JonyGamesYT9\SimpleSpawns\provider\YamlProvider;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginIdentifiableCommand;
+use pocketmine\Player;
+use function strtolower;
+
+/**
+ * Class SetLobbyCommand
+ * @package JonyGamesYT9\SimpleSpawns\commands
+ */
+class SetLobbyCommand extends Command implements PluginIdentifiableCommand 
+{
+  
+  /** @var SimpleSpawns $plugin */
+  private $plugin;
+  
+  /**
+   * SetLobbyCommand constructor.
+   * @param SimpleSpawns $plugin
+   */
+  public function __construct(SimpleSpawns $plugin) 
+  {
+    $this->plugin = $plugin;
+    parent::__construct("setlobby", "Set the spawn for users to teleport", null, []);
+  }
+  
+  /**
+   * @param CommandSender $sender
+   * @param string $label 
+   * @param array $args 
+   * @return mixed|void 
+   */
+  public function execute(CommandSender $sender, string $label, array $args) 
+  {
+    if ($sender instanceof Player) {
+      if ($sender->hasPermission("spawns.command.setlobby")) {
+        $this->getPlugin()->getYamlProvider()->setWorld($sender->getLevel()->getFolderName());
+        $this->getPlugin()->getYamlProvider()->setCoordinates("x", $sender->getX());
+        $this->getPlugin()->getYamlProvider()->setCoordinates("y", $sender->getY());
+        $this->getPlugin()->getYamlProvider()->setCoordinates("z", $sender->getZ());
+        $sender->sendMessage(strtolower(["&", "{world}", "{x}", "{y}", "{z}"], ["§", $sender->getLevel()->getFolderName(), $sender->getX(), $sender->getY(), $sender->getZ()], $this->getPlugin()->getYamlProvider()->getMessage("place.hub.success")));
+      } else {
+        $sender->sendMessage(strtolower(["&"], ["§"], $this->getPlugin()->getYamlProvider()->getMessage("no.permissions")));
+      }
+    } else {
+      $sender->sendMessage("§l§7SimpleSpawns | §r§fYou can only run this command from the game.");
+    }
+  }
+  
+  /**
+   * @return SimpleSpawns
+   */
+  public function getPlugin(): SimpleSpawns
+  {
+    return $this->plugin;
+  }
+}
